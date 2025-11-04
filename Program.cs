8de1057c3148a4b58ff73ef5a4897f0e10de38a2
Mulandi <156342676+111morris@users.noreply.github.com>
2025-11-04 23:15:42 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,17 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using 
-namespace EmailDemo 
-{
-class Program
-{
- static int Main(string[] args)
- {
-  
-  return 0;
- }
-}
-}
+using MailKit.Net.Smtp;
+using MimeKit;
+
+var message = new MimeMessage();
+message.From.Add(new MailboxAddress("[Your Name]", "[youraddress@gmail.com]"));
+message.To.Add(new MailboxAddress("[Recipient Name]", "[recipient@example.com]"));
+message.Subject = "[Testing Email]";
+message.Body = new TextPart("plain") { Text = "[Hello, this is a test email!]" };
+
+using var client = new SmtpClient();
+await client.ConnectAsync("smtp.gmail.com", 587, MailKit.Security.SecureSocketOptions.StartTls);
+await client.AuthenticateAsync("[youraddress@gmail.com]", "[yourpassword]");
+await client.SendAsync(message);
+await client.DisconnectAsync(true);
